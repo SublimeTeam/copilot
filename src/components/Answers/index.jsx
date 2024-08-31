609431a3ae20019821655d3bd96ddb517b02a64e
85ED <edsonfelixnet@gmail.com>
2024-08-30 21:37:23 -0300

    ajuste_tasks

--- src/components/Answers/index.jsx
+++ src/components/Answers/index.jsx
@@ -97,3 +97,297 @@ export const MockAnswer1 = () => {
     </p>
   );
 };
+
+export const MockAnswer2 = () => {
+  return (
+    <p>
+      Ok! Recebi as informações sobre o Ticket, e para resolver a solicitação do
+      usuário <strong>TechCorp Solutions</strong> de acordo com a Instrução de
+      Trabalho (IT) fornecida, você deve seguir os seguintes passos:
+      <br />
+      <ol>
+        <li>
+          <strong>Verificar os pré-requisitos:</strong>
+          <ul>
+            <li>
+              <strong>Nome do colaborador:</strong> Ana Oliveira.
+            </li>
+            <li>
+              <strong>Usuário do Sistema SYSTEM Z:</strong> Confirmado pelo
+              ticket.
+            </li>
+            <li>
+              <strong>Motivo da alteração do perfil:</strong> "O usuário se
+              aposentou."
+            </li>
+            <li>
+              <strong>Tipo e Grupo de acesso:</strong> Grupo de funcionários.
+            </li>
+            <li>
+              <strong>Autorização gerencial:</strong> Certifique-se de que há
+              uma autorização gerencial em anexo ao chamado.
+            </li>
+          </ul>
+        </li>
+        <li>
+          <strong>Acessar o SYSTEM Z:</strong>
+          <ul>
+            <li>
+              Clique em "Usuários" para exibir todos os usuários cadastrados no
+              sistema.
+            </li>
+          </ul>
+        </li>
+        <li>
+          <strong>Identificar o status do usuário:</strong>
+          <ul>
+            <li>
+              Verifique se o usuário Ana Oliveira está no grupo de "Ativos",
+              "Inativos" ou "Removidos":
+            </li>
+            <ul>
+              <li>
+                <strong>Ativos:</strong> Se estiver ativo, o usuário ainda tem
+                acesso ao sistema.
+              </li>
+              <li>
+                <strong>Inativos:</strong> O usuário não poderá acessar o
+                sistema.
+              </li>
+              <li>
+                <strong>Removidos:</strong> O usuário foi removido do sistema e
+                não poderá acessá-lo.
+              </li>
+            </ul>
+          </ul>
+        </li>
+        <li>
+          <strong>Remover ou desativar o usuário:</strong>
+          <ul>
+            <li>
+              Dado que o motivo da solicitação é "O usuário se aposentou", o
+              atendente deve alterar o status do usuário para "Removido",
+              conforme as diretrizes da IT para colaboradores aposentados.
+            </li>
+          </ul>
+        </li>
+        <li>
+          <strong>Registrar a alteração:</strong>
+          <ul>
+            <li>
+              Documente a alteração no Sistema de ITSM de TechCorp, conforme os
+              procedimentos padrão, incluindo evidências, se necessário.
+            </li>
+          </ul>
+        </li>
+        <li>
+          <strong>Finalizar o chamado:</strong>
+          <ul>
+            <li>
+              Feche o chamado com o status "Resolvido", confirmando que a
+              alteração foi realizada conforme solicitado.
+            </li>
+          </ul>
+        </li>
+      </ol>
+      <p>
+        Esses passos garantem que o processo seja seguido corretamente, de
+        acordo com as instruções fornecidas.
+      </p>
+    </p>
+  );
+};
+
+export const MockAnswer3 = () => {
+  return (
+    <p>
+      Ok! Recebi as informações sobre o Ticket, e para resolver a solicitação do
+      usuário <strong>Apple S/A</strong> de acordo com a Instrução de
+      Trabalho (IT) fornecida, você deve seguir os seguintes passos:
+      <br />
+      <ol>
+        <li>
+          <strong>Verificar os pré-requisitos:</strong>
+          <ul>
+            <li>
+              <strong>Nome do colaborador:</strong> Cíntia Silva.
+            </li>
+            <li>
+              <strong>Usuário do Sistema TECHNET X:</strong> Confirmado pelo
+              ticket.
+            </li>
+            <li>
+              <strong>Motivo do reset de senha do perfil:</strong> "Utilização do SAP"
+            </li>
+            <li>
+              <strong>Tipo e Grupo de acesso:</strong> Grupo de administração.
+            </li>
+            <li>
+              <strong>Autorização gerencial:</strong> Certifique-se de que há
+              uma autorização gerencial em anexo ao chamado.
+            </li>
+          </ul>
+        </li>
+        <li>
+          <strong>Acessar o TECHNET X:</strong>
+          <ul>
+            <li>
+              Clique em "Usuários" para exibir todos os usuários cadastrados no
+              sistema.
+            </li>
+          </ul>
+        </li>
+        <li>
+          <strong>Identificar o status do usuário:</strong>
+          <ul>
+            <li>
+              Verifique se o usuário Cíntia Silva está no grupo de "Ativos",
+              "Inativos" ou "Removidos":
+            </li>
+            <ul>
+              <li>
+                <strong>Ativos:</strong> Se estiver ativo, o usuário ainda tem
+                acesso ao sistema.
+              </li>
+              <li>
+                <strong>Inativos:</strong> O usuário não poderá acessar o
+                sistema.
+              </li>
+              <li>
+                <strong>Removidos:</strong> O usuário foi removido do sistema e
+                não poderá acessá-lo.
+              </li>
+            </ul>
+          </ul>
+        </li>
+        <li>
+          <strong>Atualizar o status do usuário:</strong>
+          <ul>
+            <li>
+              Dado que o motivo da solicitação é "O usuário esqueceu sua senha", o
+              atendente deve atualizar o perfil do usuário para resetar a sua senha.
+            </li>
+          </ul>
+        </li>
+        <li>
+          <strong>Registrar a alteração:</strong>
+          <ul>
+            <li>
+              Documente a alteração no Sistema de ITSM de FutureTech, conforme os
+              procedimentos padrão, incluindo evidências, se necessário.
+            </li>
+          </ul>
+        </li>
+        <li>
+          <strong>Finalizar o chamado:</strong>
+          <ul>
+            <li>
+              Feche o chamado com o status "Resolvido", confirmando que a
+              alteração foi realizada conforme solicitado.
+            </li>
+          </ul>
+        </li>
+      </ol>
+      <p>
+        Esses passos garantem que o processo seja seguido corretamente, de
+        acordo com as instruções fornecidas.
+      </p>
+    </p>
+  );
+};
+
+export const MockAnswer4 = () => {
+  return (
+    <p>
+      Ok! Recebi as informações sobre o Ticket, e para resolver a solicitação do
+      usuário <strong>GlobalTech Enterprises</strong> de acordo com a Instrução de
+      Trabalho (IT) fornecida, você deve seguir os seguintes passos:
+      <br />
+      <ol>
+        <li>
+          <strong>Verificar os pré-requisitos:</strong>
+          <ul>
+            <li>
+              <strong>Nome do colaborador:</strong> Beatriz Lima.
+            </li>
+            <li>
+              <strong>Usuário do Sistema DATAFLOW Y:</strong> Confirmado pelo
+              ticket.
+            </li>
+            <li>
+              <strong>Motivo da alteração do perfil:</strong> "Licença Office Expirada."
+            </li>
+            <li>
+              <strong>Tipo e Grupo de acesso:</strong> Grupo de desenvolvedores.
+            </li>
+            <li>
+              <strong>Autorização gerencial:</strong> Certifique-se de que há
+              uma autorização gerencial em anexo ao chamado.
+            </li>
+          </ul>
+        </li>
+        <li>
+          <strong>Acessar o SISTEMA Y:</strong>
+          <ul>
+            <li>
+            Ao acessar o SISTEMA Y, selecione a opção Manage Users.
+            </li>
+          </ul>
+        </li>
+        <li>
+          <strong>Identificar o status do usuário:</strong>
+          <ul>
+            <li>
+              Verifique se a usuária Beatriz Lima está no grupo de "Ativos",
+              "Inativos" ou "Removidos":
+            </li>
+            <ul>
+              <li>
+                <strong>Ativos:</strong> Se estiver ativo, o usuário ainda deve ter
+                acesso ao sistema.
+              </li>
+              <li>
+                <strong>Inativos:</strong> O usuário não poderá acessar o
+                sistema.
+              </li>
+              <li>
+                <strong>Removidos:</strong> O usuário foi removido do sistema e
+                não poderá acessá-lo.
+              </li>
+            </ul>
+          </ul>
+        </li>
+        <li>
+          <strong>Atualizar o status do usuário:</strong>
+          <ul>
+            <li>
+            Clicar em “Edit User”:O parâmetro que define um usuário como ativo ou não é o parâmetro Enabled¸ caso o mesmo esteja marcado o usuário está ativo, caso esteja desmarcado o usuário está inativo. 
+            </li>
+          </ul>
+        </li>
+        <li>
+          <strong>Registrar a alteração:</strong>
+          <ul>
+            <li>
+              Documente a alteração no Sistema de ITSM de GlobalTech, conforme os
+              procedimentos padrão, incluindo evidências, se necessário.
+            </li>
+          </ul>
+        </li>
+        <li>
+          <strong>Finalizar o chamado:</strong>
+          <ul>
+            <li>
+              Feche o chamado com o status "Resolvido", confirmando que a
+              alteração foi realizada conforme solicitado.
+            </li>
+          </ul>
+        </li>
+      </ol>
+      <p>
+        Esses passos garantem que o processo seja seguido corretamente, de
+        acordo com as instruções fornecidas.
+      </p>
+    </p>
+  );
+};
