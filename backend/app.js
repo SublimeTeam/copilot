f81f6fc3b8c447d9b7a718a735f972961790cffd
LeandrodeLimaC <leandrolima7472@gmail.com>
2024-10-23 22:23:25 -0300

    improve prompts for IAs and support csv files

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,12 @@ const { HfInference } = require("@huggingface/inference");
 const dotenv = require("dotenv");
 const mammoth = require("mammoth");
 const cors = require("cors"); // Import the cors package
+const csv = require("csv-parser"); // Add this line to import the csv-parser package
+const fs = require("fs"); // Add this line to handle file system operations
+const { defaultChats } = require("./data");
 
 // TODO: persistir chats em banco de dados
-const chats = [];
+const chats = defaultChats;
 
 const PORT = process.env.PORT || 3000;
 
@@ -31,7 +34,7 @@ const chatAi = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 const chatAIModel = chatAi.getGenerativeModel({
   model: "gemini-1.5-flash",
   generationConfig: {
-    temperature: 1,
+    temperature: 0.7,
     maxOutputTokens: 5000,
   },
 });
@@ -56,11 +59,11 @@ const hf = new HfInference(process.env.HUGGINGFACE_API_KEY);
 // Função para criar embeddings usando Hugging Face
 async function createEmbedding(text) {
   const prompt = `
-    You are an assistant tasked with summarizing tables and text for retrieval.
+    You are an assistant tasked with summarizing tables, text or JSON for retrieval.
     These summaries will be embedded and used for retrieve the raw text or table elements.
     Give a concise summary of the table or text that is well optimized for retrieval.
     Use the same language as the text.
-    Table or text: ${text}
+    Table, text or JSON: ${text}
   `;
 
   const result = await flashAIModel.generateContent(prompt);
@@ -97,6 +100,8 @@ async function createEmbedding(text) {
 }
 
 // Endpoint para upload de arquivos
+// add support for csv files
+
 app.post("/upload", upload.single("file"), async (req, res) => {
   console.log("Request received:", req.body);
   console.log("File object:", req.file);
@@ -107,10 +112,27 @@ app.post("/upload", upload.single("file"), async (req, res) => {
 
   try {
     console.log("File path:", req.file.path);
-    // docx to text
-    const result = await mammoth.extractRawText({ path: req.file.path });
+    // Check if the file is a docx or a csv
+    const fileExtension = req.file.originalname.split(".").pop();
+    let textContent;
+    if (fileExtension === "docx") {
+      const result = await mammoth.extractRawText({ path: req.file.path });
+      textContent = result.value;
+    } else if (fileExtension === "csv") {
+      textContent = await new Promise((resolve, reject) => {
+        const results = [];
+        fs.createReadStream(req.file.path)
+          .pipe(csv())
+          .on("data", (data) => results.push(data))
+          .on("end", () => {
+            resolve(JSON.stringify(results)); // Convert CSV rows to JSON string
+          })
+          .on("error", (error) => reject(error));
+      });
 
-    const textContent = result.value;
+      console.log("CSV content:", textContent);
+    }
+
     console.log("Extracted text content length:", textContent.length);
 
     const embedding = await createEmbedding(textContent);
@@ -154,7 +176,7 @@ app.post("/message", express.json(), async (req, res) => {
 
     const queryResponse = await index.query({
       vector: messageEmbedding,
-      topK: 3,
+      topK: 1,
       includeMetadata: true,
     });
 
@@ -170,7 +192,7 @@ app.post("/message", express.json(), async (req, res) => {
       const prompt = `
         Create a title for the chat based on the user message provided.
         Answer in plain text without any markdown formatting or symbols.
-        Use the same language as the message.
+        Use the same language from the chat history.
         User message: ${optimizedMessage}
       `;
 
@@ -192,7 +214,7 @@ app.post("/message", express.json(), async (req, res) => {
 
     const combinedContext = `
       Answer the question or message in a natural and conversational manner. Use the provided context to inform your response when applicable, but feel free to engage in general conversation when the message is not a question or does not require specific context.
-      You should always use INSTRUÇÃO DE TRABALHO (IT) as a base to answer questions related to work instructions providing as much information as possible, but for general conversation, respond appropriately.
+      You should always use the Context information as a base to answer questions related to work instructions providing as much information as possible, but for general conversation, respond appropriately.
       Use the same language of the question. and avoid talking about random topics.
       Never ask the user to contact the support.
 
@@ -202,7 +224,7 @@ app.post("/message", express.json(), async (req, res) => {
       
       Question/Message: ${optimizedMessage}
       
-      Answer in markdown format, using bold, italic, and other markdown formatting options to make the response more readable. Make sure to use a valid markdown formatting for the response. and substitute space scape for three spaces to create a new line.
+      Answer in markdown format, using bold, italic, and other markdown formatting options to make the response more readable NEVER use html tags. Make sure to use a valid markdown formatting for the response. and substitute space scape for three spaces to create a new line.
       Answer:
     `;
 
@@ -219,13 +241,20 @@ app.post("/message", express.json(), async (req, res) => {
 
     const chatHistoryWithResponse = `${chatHistory}\n${response.text()}`;
 
+    // create
     const suggestionContext = `
-      Give comma separated quick questions based on the context provided. Max 5 questions.
-      Only return questions that you know the answer to. If you dont have any questions, just return an empty string.
-      Be to the point. Don't use full sentences. Use plain text.
-      Use the same language from the chat history.
-      
+      Generate up to 4 relevant questions that the user may ask you, based on the context.
+      Only include questions about content from the context.
+      If you can't generate any suggestions, return an empty string. Keep it concise, no full sentences, use plain text. 
+      All suggestions should be comma-separated and start with the first word capitalized.
+      Never include questions that was already asked or answered in the chat history.
+
+      Context: ${fileContext}
+
       Chat history: ${chatHistoryWithResponse}
+
+      Answer should match the chat history language
+      Answer:
     `;
 
     try {
